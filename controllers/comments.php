6cc5768deb7c2e6e9a39201955d965e6752559cd
Ferenc Szekely <ferenc.szekely@gmail.com>
2012-01-31 17:31:25 +0100

    refactored comment retrieval; added utf-8 char encoding into the HTTP header

--- controllers/comments.php
+++ controllers/comments.php
@@ -9,7 +9,7 @@ class com_meego_ocs_controllers_comments
         $this->request = $request;
     }
 
-    public function get_get(array $args)
+    public function get_comments(array $args)
     {
         if ($args['type'] != 1)
         {
@@ -19,24 +19,26 @@ class com_meego_ocs_controllers_comments
         $primary = new com_meego_package();
         $primary->get_by_id((int) $args['contentid1']);
 
-        $storage = new midgard_query_storage('com_meego_comments_comment');
+        $storage = new midgard_query_storage('com_meego_package_ratings');
         $q = new midgard_query_select($storage);
 
         $qc = new midgard_query_constraint_group('AND');
 
         $qc->add_constraint(new midgard_query_constraint(
-            new midgard_query_property('metadata.creator'),
+            new midgard_query_property('authorguid'),
             '<>',
             new midgard_query_value('')
         ));
-        $qc->add_constraint(new midgard_query_constraint(
-            new midgard_query_property('to'),
+
+        $qc->add_constraint(new midgard_query_constraint (
+            new midgard_query_property('name'),
             '=',
-            new midgard_query_value($primary->guid)
+            new midgard_query_value($primary->name)
         ));
 
         $q->set_constraint($qc);
-        $q->add_order(new midgard_query_property('metadata.created', $storage), SORT_ASC);
+
+        $q->add_order(new midgard_query_property('posted', $storage), SORT_ASC);
 
         // First run a query of the whole set to get results count
         $q->execute();
@@ -51,16 +53,33 @@ class com_meego_ocs_controllers_comments
         $comments = $q->list_objects();
 
         $ocs = new com_meego_ocs_OCSWriter();
-        $ocs->writeMeta($cnt);
-
-        $ocs->startElement('data');
 
         foreach ($comments as $comment)
         {
+            if (   $comment->commentid == 0
+                && ! midgardmvc_core::get_instance()->configuration->show_ratings_without_comments)
+            {
+                // skip the rating if it has no comment and the configuration excludes such ratings
+                --$cnt;
+            }
+        }
+
+        $ocs->writeMeta($cnt);
+        $ocs->startElement('data');
+
+        // todo: again this loop..  a bit redundant, but works for now
+        foreach ($comments as $comment)
+        {
+            if (   $comment->commentid == 0
+                && ! midgardmvc_core::get_instance()->configuration->show_ratings_without_comments)
+            {
+                // skip the rating if it has no comment and the configuration excludes such ratings
+                continue;
+            }
             $this->comment_to_ocs($comment, $ocs);
         }
-        $ocs->endElement();
 
+        $ocs->endElement();
         $ocs->endDocument();
 
         self::output_xml($ocs);
@@ -85,36 +104,26 @@ class com_meego_ocs_controllers_comments
         return array($pagesize, $page * $pagesize);
     }
 
-    private function comment_to_ocs(com_meego_comments_comment $comment, com_meego_ocs_OCSWriter $ocs)
+    private function comment_to_ocs(com_meego_package_ratings $rating, com_meego_ocs_OCSWriter $ocs)
     {
         $ocs->startElement('comment');
-        $ocs->writeElement('id', $comment->id);
-        $ocs->writeElement('subject', $comment->title);
-        $ocs->writeElement('text', $comment->content);
+        $ocs->writeElement('id', $rating->commentid);
+        $ocs->writeElement('subject', $rating->title);
+        $ocs->writeElement('text', $rating->comment);
 
-        // no support of subcomments yet
-        /*
-        $comments = count($comment->subcomments);
-        $ocs->writeElement('childcount', $comments);
-        if ($comments > 0)
-        {
-            $ocs->startElement('children');
-            foreach ($comment->subcomments as $subcomment)
-            {
-                $this->comment_to_ocs($subcomment, $ocs);
-            }
-            $ocs->endElement();
-        }
-        */
+        // todo: no support of subcomments yet
+
         $userid = '';
-        $user = com_meego_packages_utils::get_user_by_person_guid($comment->metadata->creator);
+        $user = com_meego_packages_utils::get_user_by_person_guid($rating->authorguid);
+
         if ($user)
         {
             $userid = $user->login;
         }
+
         $ocs->writeElement('user', $userid);
-        $ocs->writeElement('date', $comment->metadata->created->format('c'));
-        $ocs->writeElement('score', $comment->metadata->score);
+        $ocs->writeElement('date', $rating->posted->format('c'));
+        $ocs->writeElement('score', $rating->rating);
         $ocs->endElement();
     }
 
@@ -216,7 +225,7 @@ class com_meego_ocs_controllers_comments
 
     private static function output_xml($xml)
     {
-        midgardmvc_core::get_instance()->dispatcher->header('Content-type: application/xml');
+        midgardmvc_core::get_instance()->dispatcher->header('Content-type: application/xml; charset=utf-8');
         echo $xml->outputMemory(true);
 
         midgardmvc_core::get_instance()->dispatcher->end_request();
